9895109744bf62d2cccdd027d1286845df31b0da
PRITOM-0 <pritomsahawww@gmail.com>
2026-04-13 14:41:09 +0600

    Initial Setup

---
+++ FRONTEND/src/App.jsx
@@ -0,0 +1,17 @@
+
+import Home from "./pages/Home";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import ProductDetails from "./pages/ProductDetails";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/login", element: <Login /> },
+  { path: "/signup", element: <Signup /> },
+  { path: "/product/:id", element: <ProductDetails /> },
+]);
+
+export default function App() {
+  return <RouterProvider router={router} />;
+}
